5026eef1ad117b60517faa4552cea527b8ff26d4
rahul74747474 <ghibli141525@gmail.com>
2026-01-29 13:58:51 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ console.log("🔥 app.js LOADED");
 // Middlewares
 app.use(
   cors({
-    origin: "http://localhost:8080",
+    origin: "https://dg-frontend-two.vercel.app/",
     credentials: true,
   })
 );
@@ -38,3 +38,4 @@ app.use((req, res) => {
 });
 
 export default app;
+
